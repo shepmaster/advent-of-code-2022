d79ccf6f14fa53c43cabc4abd518aa0345a2795c
Jake Goulding <jake.goulding@gmail.com>
2022-12-04 08:57:39 -0500

    day04 - part 2

--- day-04/src/main.rs
+++ day-04/src/main.rs
@@ -7,6 +7,7 @@ use std::{num::ParseIntError, ops::RangeInclusive};
 const INPUT: &str = include_str!("../input");
 
 type Id = u32;
+type Elf = RangeInclusive<Id>;
 
 #[snafu::report]
 fn main() -> Result<()> {
@@ -14,25 +15,34 @@ fn main() -> Result<()> {
     println!("{part1}");
     assert_eq!(444, part1);
 
+    let part2 = count_of_any_contained_pairs(INPUT)?;
+    println!("{part2}");
+    assert_eq!(801, part2);
+
     Ok(())
 }
 
 fn count_of_fully_contained_pairs(s: &str) -> Result<usize> {
-    let i = s
-        .lines()
-        .map(|l| {
-            let (e1, e2) = l.split_once(',').context(MissingPairSnafu)?;
-            let [e1, e2] =
-                [e1, e2].try_map(|e| e.split_once('-').context(MissingRangePartSnafu))?;
-            [e1, e2].try_map(|(s, e)| {
-                let [s, e] =
-                    [s, e].try_map(|id| id.parse::<Id>().context(InvalidIdSnafu { id }))?;
-                Ok(s..=e)
-            })
-        })
-        .filter_ok(|[e1, e2]| either_fully_contains(e1, e2));
+    let pairs = parse_elf_pairs(s).filter_ok(|[e1, e2]| either_fully_contains(e1, e2));
 
-    itertools::process_results(i, |i| i.count())
+    itertools::process_results(pairs, |i| i.count())
+}
+
+fn count_of_any_contained_pairs(s: &str) -> Result<usize> {
+    let pairs = parse_elf_pairs(s).filter_ok(|[e1, e2]| either_any_contains(e1, e2));
+
+    itertools::process_results(pairs, |i| i.count())
+}
+
+fn parse_elf_pairs(s: &str) -> impl Iterator<Item = Result<[Elf; 2]>> + '_ {
+    s.lines().map(|l| {
+        let (e1, e2) = l.split_once(',').context(MissingPairSnafu)?;
+        let [e1, e2] = [e1, e2].try_map(|e| e.split_once('-').context(MissingRangePartSnafu))?;
+        [e1, e2].try_map(|(s, e)| {
+            let [s, e] = [s, e].try_map(|id| id.parse::<Id>().context(InvalidIdSnafu { id }))?;
+            Ok(s..=e)
+        })
+    })
 }
 
 fn either_fully_contains<T>(a: &RangeInclusive<T>, b: &RangeInclusive<T>) -> bool
@@ -47,6 +57,13 @@ where
     T: Ord,
 {
     a.start() >= b.start() && a.end() <= b.end()
+}
+
+fn either_any_contains<T>(a: &RangeInclusive<T>, b: &RangeInclusive<T>) -> bool
+where
+    T: Ord,
+{
+    a.contains(b.start()) || a.contains(b.end()) || b.contains(a.start()) || b.contains(a.end())
 }
 
 #[derive(Debug, Snafu)]
@@ -70,6 +87,13 @@ mod test {
     #[snafu::report]
     fn example() -> Result<()> {
         assert_eq!(2, count_of_fully_contained_pairs(INPUT)?);
+        Ok(())
+    }
+
+    #[test]
+    #[snafu::report]
+    fn example_part2() -> Result<()> {
+        assert_eq!(4, count_of_any_contained_pairs(INPUT)?);
         Ok(())
     }
 }
