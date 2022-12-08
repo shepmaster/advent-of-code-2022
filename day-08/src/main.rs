ab2e4c1c2e25c329c71f78c5d7354fd110fbc4c5
Jake Goulding <jake.goulding@gmail.com>
2022-12-08 13:57:41 -0500

    day08 - part 2

--- day-08/src/main.rs
+++ day-08/src/main.rs
@@ -1,6 +1,9 @@
 use itertools::Itertools;
 use snafu::prelude::*;
-use std::collections::{BTreeMap, BTreeSet};
+use std::{
+    collections::{BTreeMap, BTreeSet},
+    ops::ControlFlow,
+};
 
 const INPUT: &str = include_str!("../input");
 
@@ -9,6 +12,10 @@ fn main() -> Result<()> {
     let part1 = visible_trees(INPUT)?;
     println!("{part1}");
     assert_eq!(1792, part1);
+
+    let part2 = maximum_scenic_score(INPUT)?;
+    println!("{part2}");
+    assert_eq!(334880, part2);
 
     Ok(())
 }
@@ -35,6 +42,31 @@ fn visible_trees(s: &str) -> Result<usize> {
     Ok(visible.len())
 }
 
+fn maximum_scenic_score(s: &str) -> Result<usize> {
+    let forest = parse_forest(s)?;
+    let ((min_x, min_y), (max_x, max_y)) = forest_bounds(&forest).context(ForestIsEmptySnafu)?;
+
+    let inner_coords = (min_y + 1..=max_y - 1).cartesian_product(min_x + 1..=max_x - 1);
+
+    inner_coords
+        .map(|(y, x)| {
+            let my_height = forest[&(x, y)];
+
+            let top = (min_y..y)
+                .rev()
+                .count_trees(|&y| forest[&(x, y)] < my_height);
+            let bottom = (y + 1..=max_y).count_trees(|&y| forest[&(x, y)] < my_height);
+            let left = (min_x..x)
+                .rev()
+                .count_trees(|&x| forest[&(x, y)] < my_height);
+            let right = (x + 1..=max_x).count_trees(|&x| forest[&(x, y)] < my_height);
+
+            top * bottom * left * right
+        })
+        .max()
+        .context(ForestIsEmptySnafu)
+}
+
 type Coord = (usize, usize);
 type Height = u32;
 type Forest = BTreeMap<Coord, Height>;
@@ -57,6 +89,35 @@ fn forest_bounds(forest: &Forest) -> Option<(Coord, Coord)> {
     Some((min, max))
 }
 
+trait ForestExt: Iterator {
+    fn count_trees<P>(self, pred: P) -> usize
+    where
+        P: FnMut(&Self::Item) -> bool;
+}
+
+impl<I> ForestExt for I
+where
+    I: Iterator,
+{
+    fn count_trees<P>(mut self, mut pred: P) -> usize
+    where
+        P: FnMut(&Self::Item) -> bool,
+    {
+        let visible = self.try_fold(0, |acc, y| {
+            if pred(&y) {
+                ControlFlow::Continue(acc + 1)
+            } else {
+                ControlFlow::Break(acc)
+            }
+        });
+
+        match visible {
+            ControlFlow::Continue(v) => v,  // ran out of forest
+            ControlFlow::Break(v) => v + 1, // hit a tree
+        }
+    }
+}
+
 #[derive(Debug, Snafu)]
 enum Error {
     InvalidHeight { x: usize, y: usize, c: char },
@@ -76,6 +137,13 @@ mod test {
     #[snafu::report]
     fn example() -> Result<()> {
         assert_eq!(21, visible_trees(INPUT)?);
+        Ok(())
+    }
+
+    #[test]
+    #[snafu::report]
+    fn example_part2() -> Result<()> {
+        assert_eq!(8, maximum_scenic_score(INPUT)?);
         Ok(())
     }
 }
