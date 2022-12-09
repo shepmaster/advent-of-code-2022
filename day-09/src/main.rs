59533289915513c5d7fd4a2459fa0f5765c4e1d4
Jake Goulding <jake.goulding@gmail.com>
2022-12-09 12:37:39 -0500

    day09 - part 2

--- day-09/src/main.rs
+++ day-09/src/main.rs
@@ -1,3 +1,5 @@
+#![feature(get_many_mut)]
+
 use snafu::prelude::*;
 use std::{collections::BTreeSet, str::FromStr};
 
@@ -5,15 +7,19 @@ const INPUT: &str = include_str!("../input");
 
 #[snafu::report]
 fn main() -> Result<()> {
-    let part1 = unique_tail_positions(INPUT)?;
+    let part1 = unique_tail_positions::<2>(INPUT)?;
     println!("{part1}");
     assert_eq!(6384, part1);
+
+    let part2 = unique_tail_positions::<10>(INPUT)?;
+    println!("{part2}");
+    assert_eq!(2734, part2);
 
     Ok(())
 }
 
-fn unique_tail_positions(s: &str) -> Result<usize> {
-    let mut state = State::default();
+fn unique_tail_positions<const N: usize>(s: &str) -> Result<usize> {
+    let mut state = State::<N>::default();
 
     for line in s.lines() {
         let command = line.parse::<Command<u8>>().context(InvalidCommandSnafu)?;
@@ -26,25 +32,23 @@ fn unique_tail_positions(s: &str) -> Result<usize> {
 type Coord = (i32, i32);
 
 #[derive(Debug)]
-struct State {
-    head: Coord,
-    tail: Coord,
+struct State<const N: usize> {
+    knots: [Coord; N],
     tail_visited: BTreeSet<Coord>,
 }
 
-impl Default for State {
+impl<const N: usize> Default for State<N> {
     fn default() -> Self {
         let mut me = Self {
-            head: Default::default(),
-            tail: Default::default(),
+            knots: [(0, 0); N],
             tail_visited: Default::default(),
         };
-        me.tail_visited.insert(me.tail);
+        me.tail_visited.extend(me.knots.last().copied());
         me
     }
 }
 
-impl State {
+impl<const N: usize> State<N> {
     fn tail_visited(&self) -> usize {
         self.tail_visited.len()
     }
@@ -52,38 +56,51 @@ impl State {
     fn move_once(&mut self, command: Command<()>) -> Result<()> {
         use Command::*;
 
-        match command {
-            Up(()) => self.head.1 += 1,
-            Down(()) => self.head.1 -= 1,
-            Left(()) => self.head.0 -= 1,
-            Right(()) => self.head.0 += 1,
-        }
+        let indices = 0..N;
+        let head_indices = indices.clone();
+        let tail_indices = indices.skip(1);
+        let head_tail_indices = head_indices.zip(tail_indices);
 
-        match self.delta().context(LeftTheBoardSnafu)? {
-            #[rustfmt::skip]
-            (-1,  1) | ( 0,  1) | ( 1,  1) |
-            (-1,  0) | ( 0,  0) | ( 1,  0) |
-            (-1, -1) | ( 0, -1) | ( 1, -1) => { /* Close enough */ }
-
-            (dx, dy) => {
-                assert!((-2..=2).contains(&dx));
-                assert!((-2..=2).contains(&dy));
-
-                self.tail.0 += dx.signum();
-                self.tail.1 += dy.signum();
+        if let Some(head) = self.knots.first_mut() {
+            match command {
+                Up(()) => head.1 += 1,
+                Down(()) => head.1 -= 1,
+                Left(()) => head.0 -= 1,
+                Right(()) => head.0 += 1,
             }
         }
 
-        self.tail_visited.insert(self.tail);
+        for (head_index, tail_index) in head_tail_indices {
+            let [head, tail] = self
+                .knots
+                .get_many_mut([head_index, tail_index])
+                .expect("Created overlapping indices");
+
+            match delta(*head, *tail).context(LeftTheBoardSnafu)? {
+                #[rustfmt::skip]
+                (-1,  1) | ( 0,  1) | ( 1,  1) |
+                (-1,  0) | ( 0,  0) | ( 1,  0) |
+                (-1, -1) | ( 0, -1) | ( 1, -1) => { /* Close enough */ }
+
+                (dx, dy) => {
+                    assert!((-2..=2).contains(&dx));
+                    assert!((-2..=2).contains(&dy));
+
+                    tail.0 += dx.signum();
+                    tail.1 += dy.signum();
+                }
+            }
+        }
+
+        self.tail_visited.extend(self.knots.last().copied());
         Ok(())
     }
+}
 
-    fn delta(&self) -> Option<Coord> {
-        let Self { head, tail, .. } = *self;
-        let x = head.0.checked_sub(tail.0)?;
-        let y = head.1.checked_sub(tail.1)?;
-        Some((x, y))
-    }
+fn delta(head: Coord, tail: Coord) -> Option<Coord> {
+    let x = head.0.checked_sub(tail.0)?;
+    let y = head.1.checked_sub(tail.1)?;
+    Some((x, y))
 }
 
 #[derive(Debug, Copy, Clone)]
@@ -153,11 +170,26 @@ mod test {
     use super::*;
 
     const INPUT: &str = include_str!("../input.test");
+    const INPUT2: &str = include_str!("../input2.test");
 
     #[test]
     #[snafu::report]
     fn example() -> Result<()> {
-        assert_eq!(13, unique_tail_positions(INPUT)?);
+        assert_eq!(13, unique_tail_positions::<2>(INPUT)?);
+        Ok(())
+    }
+
+    #[test]
+    #[snafu::report]
+    fn example_part2() -> Result<()> {
+        assert_eq!(1, unique_tail_positions::<10>(INPUT)?);
+        Ok(())
+    }
+
+    #[test]
+    #[snafu::report]
+    fn example_part2_input2() -> Result<()> {
+        assert_eq!(36, unique_tail_positions::<10>(INPUT2)?);
         Ok(())
     }
 }
