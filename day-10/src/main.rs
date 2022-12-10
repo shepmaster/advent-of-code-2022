ef112c938e771f363d66115a28044e16168f8336
Jake Goulding <jake.goulding@gmail.com>
2022-12-10 13:45:31 -0500

    day10 - part 2

--- day-10/src/main.rs
+++ day-10/src/main.rs
@@ -4,6 +4,9 @@ use std::{iter, str::FromStr};
 
 const ADD_X_DURATION: u8 = 2;
 
+const DISPLAY_WIDTH: u8 = 40;
+const DISPLAY_HEIGHT: u8 = 6;
+
 const INPUT: &str = include_str!("../input");
 
 #[snafu::report]
@@ -12,17 +15,62 @@ fn main() -> Result<()> {
     println!("{part1}");
     assert_eq!(15140, part1);
 
+    let part2 = render_signal(INPUT)?;
+    println!("{part2}");
+    assert_eq!(
+        "###..###....##..##..####..##...##..###..\n\
+         #..#.#..#....#.#..#....#.#..#.#..#.#..#.\n\
+         ###..#..#....#.#..#...#..#....#..#.#..#.\n\
+         #..#.###.....#.####..#...#.##.####.###..\n\
+         #..#.#....#..#.#..#.#....#..#.#..#.#....\n\
+         ###..#.....##..#..#.####..###.#..#.#....\n",
+        part2,
+    );
+
     Ok(())
 }
 
 fn sum_of_six_signal_strengths(s: &str) -> Result<i32> {
+    x_values(s)
+        .skip(20 - 1) // One based cycle indexing
+        .step_by(40)
+        .take(6)
+        .map_ok(|(i, x)| i32::from(i) * x)
+        .sum()
+}
+
+fn render_signal(s: &str) -> Result<String> {
+    let mut rendered = String::new();
+    let mut crt_x_position = (0..i32::from(DISPLAY_WIDTH)).cycle();
+
+    let n_pixels = usize::from(DISPLAY_WIDTH) * usize::from(DISPLAY_HEIGHT);
+
+    for x_value in x_values(s).take(n_pixels) {
+        let (_cycle, x_value) = x_value?;
+        let crt_x_position = crt_x_position.next().expect("Infinite iterator ended");
+        let pixel_range = x_value - 1..=x_value + 1;
+        let pixel = if pixel_range.contains(&crt_x_position) {
+            '#'
+        } else {
+            '.'
+        };
+        rendered.push(pixel);
+        if crt_x_position == 39 {
+            rendered.push('\n');
+        }
+    }
+
+    Ok(rendered)
+}
+
+fn x_values(s: &str) -> impl Iterator<Item = Result<(u16, i32)>> + '_ {
     let mut instructions = s.lines().map(Instruction::from_str).fuse();
 
     let mut cycle = 1u16;
     let mut x = 1;
     let mut cached_add_x = None;
 
-    let x_values = iter::from_fn(|| {
+    iter::from_fn(move || {
         let orig_cycle = cycle;
         let orig_x = x;
 
@@ -53,14 +101,7 @@ fn sum_of_six_signal_strengths(s: &str) -> Result<i32> {
         cycle += 1;
 
         Some(Ok((orig_cycle, orig_x)))
-    });
-
-    x_values
-        .skip(20 - 1) // One based cycle indexing
-        .step_by(40)
-        .take(6)
-        .map_ok(|(i, x)| i32::from(i) * x)
-        .sum()
+    })
 }
 
 #[derive(Debug, Copy, Clone, Default)]
@@ -124,6 +165,21 @@ mod test {
     #[snafu::report]
     fn example() -> Result<()> {
         assert_eq!(13140, sum_of_six_signal_strengths(INPUT)?);
+        Ok(())
+    }
+
+    #[test]
+    #[snafu::report]
+    fn example_part2() -> Result<()> {
+        assert_eq!(
+            "##..##..##..##..##..##..##..##..##..##..\n\
+             ###...###...###...###...###...###...###.\n\
+             ####....####....####....####....####....\n\
+             #####.....#####.....#####.....#####.....\n\
+             ######......######......######......####\n\
+             #######.......#######.......#######.....\n",
+            render_signal(INPUT)?,
+        );
         Ok(())
     }
 }
